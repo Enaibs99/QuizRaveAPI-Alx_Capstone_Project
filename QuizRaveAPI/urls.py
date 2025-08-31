cc99bf03ffac3f4b3c7066a07a219f0d906f5a99
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-08-31 15:55:46 +0100

    added the missing path in main url

--- QuizRaveAPI/urls.py
+++ QuizRaveAPI/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from quiz import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('quiz.urls')),
+    path('', views.home, name='home'), 
 ]
